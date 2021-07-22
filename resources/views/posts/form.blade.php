1d5bd72d054e201ddba544e463ee24d3ba0e5d26
Ubuntu <ubuntu@ip-172-31-26-151.ec2.internal>
2021-07-22 14:53:56 +0000

    layout

--- resources/views/posts/form.blade.php
+++ resources/views/posts/form.blade.php
@@ -1,7 +1,9 @@
 {!! Form::open(['route' => 'posts.store']) !!}
     <div class="form-group">
+        {!! Form::label('content', '単語:') !!}
         {!! Form::textarea('word', null, ['class' => 'form-control', 'rows' => '2']) !!}
+        {!! Form::label('content', '日本語訳:') !!}
         {!! Form::textarea('japanese', null, ['class' => 'form-control', 'rows' => '2']) !!}
-        {!! Form::submit('Post', ['class' => 'btn btn-primary btn-block']) !!}
+        {!! Form::submit('投稿', ['class' => 'btn btn-primary btn-block']) !!}
     </div>
 {!! Form::close() !!}
